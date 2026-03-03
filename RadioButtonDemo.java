86f512e0b3c2024804d810b0be28e309f5406e50
markthairug <markthairug@gmail.com>
2026-03-03 14:30:42 +0300

    Update and rename RadioButton.java to RadioButtonDemo.java

--- RadioButtonDemo.java
+++ RadioButtonDemo.java
@@ -2,7 +2,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class RadioButton extends JFrame implements ActionListener {
+public class RadioButtonDemo extends JFrame implements ActionListener {
 
     // Radio buttons
     private final JRadioButton bird, cat, dog, rabbit, pig;
@@ -89,6 +89,4 @@ public class RadioButton extends JFrame implements ActionListener {
         // Run GUI in Event Dispatch Thread
         SwingUtilities.invokeLater(() -> new RadioButtonDemo());
     }
-
 }
-
